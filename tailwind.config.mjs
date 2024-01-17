489fd7c105970700a3c55254aa03d82a67e93bde
Zairanww <zliu11@risd.edu>
2024-01-16 23:09:39 -0500

    re

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily:{
-      'title':['Suisse Works'],
+      'title':['"Untitled Sans"'],
       'body':['skolar-sans-pe','sans-serif']
     },
     extend: {},
